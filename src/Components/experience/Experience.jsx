6e24877701c4f92607899dc2b73a3d8c304c18a2
MAYURESHBAILURKAR <mayureshbailurkar@gmail.com>
2022-09-17 03:12:38 +0530

    added

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -1,44 +1,59 @@
 import React from "react";
 import "./experience.css";
 import { HiCheckCircle } from "react-icons/hi";
+import { FaNodeJs, FaReact } from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { SiChakraui, SiExpress, SiMongodb, SiRedux } from "react-icons/si";
+import { ImHtmlFive } from "react-icons/im";
+import { DiCss3 } from "react-icons/di";
+
 const Experience = () => {
   const skillsFront = [
     {
+      image: <ImHtmlFive />,
       titile: "HTML",
       status: "Experienced",
     },
     {
+      image: <DiCss3 />,
       titile: "CSS",
       status: "Experienced",
     },
     {
+      image: <FaReact />,
       titile: "REACT",
       status: "Experienced",
     },
     {
+      image: <DiJavascript1 />,
       titile: "JAVASCRIPT",
       status: "Experienced",
     },
     {
+      image: <SiChakraui />,
       titile: "CHAKRA UI",
       status: "Intermediate",
     },
     {
+      image: <SiRedux />,
       titile: "Redux",
-      status: "Experienced",
+      status: "Intermediate",
     },
   ];
   // =================================BackEnd
   const skillsBack = [
     {
+      image: <FaNodeJs />,
       titile: "NodeJS",
       status: "Intermediate",
     },
     {
+      image: <SiExpress />,
       titile: "ExpressJS",
       status: "Intermediate",
     },
     {
+      image: <SiMongodb />,
       titile: "MongoDb",
       status: "Intermediate",
     },
@@ -53,7 +68,12 @@ const Experience = () => {
           <div className="experience_content">
             {skillsFront.map((el) => (
               <article className="experience_details">
-                <HiCheckCircle className="experience_details_icon" />
+                <div>
+                  <div className="experience_details_skill_icon">
+                    {el.image}
+                  </div>
+                  <HiCheckCircle className="experience_details_icon" />
+                </div>
                 <div>
                   <h4>{el.titile}</h4>
                   <small className="text-light">{el.status}</small>
@@ -67,7 +87,12 @@ const Experience = () => {
           <div className="experience_content">
             {skillsBack.map((el) => (
               <article className="experience_details">
-                <HiCheckCircle className="experience_details_icon" />
+                <div>
+                  <div className="experience_details_skill_icon">
+                    {el.image}
+                  </div>
+                  <HiCheckCircle className="experience_details_icon" />
+                </div>
                 <div>
                   <h4>{el.titile}</h4>
                   <small className="text-light">{el.status}</small>
